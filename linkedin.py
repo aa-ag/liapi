f4d76a7b51935c1a8e81cff25e5a9228e276a841
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-27 13:52:25 -0500

    added headers and tried passing them as header in response; still not working

--- linkedin.py
+++ linkedin.py
@@ -1,16 +1,22 @@
 ############------------ IMPORTS ------------############
 import requests
+from requests.api import head
 import settings
 
 ############------------ GLOBAL VARIABLE(S) ------------############
 authorization_code = settings.authorization_code
+
+headers = {'Content-Type': 'application/json',
+           'X-Restli-Protocol-Version': '2.0.0',
+           'Authorization': f'Bearer {authorization_code}'}
+           
            
 ############------------ FUNCTION(S) ------------############
 def get_user_info():
     '''
      hit endpoint and retreive user info
     '''
-    response = requests.get('https://api.linkedin.com/v2/me')
+    response = requests.get('https://api.linkedin.com/v2/me', headers=headers)
 
     print(response.status_code)
 
