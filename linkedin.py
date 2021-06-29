5e8232b2786fce43e4f562ac847620733ef0c9cb
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-29 18:20:48 -0500

    refactored linked.py and added docstring in generate_authorization_url()

--- linkedin.py
+++ linkedin.py
@@ -6,8 +6,18 @@ import settings
 
 ############------------ FUNCTION(S) ------------############
 def generate_authorization_url():
-    url = f'https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id={settings.client_id}&redirect_uri={settings.linkedin_redirect}&state={secrets.token_hex(8).upper()}&scope=r_liteprofile%20r_emailaddress%20w_member_social'
-    print(url)
+    '''
+     prepare each element of the url necessary to
+     retreive authorization code according to
+     documentation: https://tinyurl.com/5umjmm42
+    '''
+    a = 'https://www.linkedin.com/oauth/v2/authorization?'
+    b = 'response_type=code&'
+    c = f'client_id={settings.client_id}&'
+    d = f'redirect_uri={settings.linkedin_redirect}&'
+    e = f'state={secrets.token_hex(8).upper()}&'
+    f = 'scope=r_liteprofile%20r_emailaddress%20w_member_social'
+    print(a + b + c + d + e + f)
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
