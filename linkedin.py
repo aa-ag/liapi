05bfff0ca7e7a9b4cd96464a894ed0ea6e860613
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-29 18:14:19 -0500

    successfully generated authorization url and manually authorized app

--- linkedin.py
+++ linkedin.py
@@ -1,22 +1,14 @@
 ############------------ IMPORTS ------------############
 import requests
-from requests.api import head
+import secrets
 import settings
 
-############------------ GLOBAL VARIABLE(S) ------------############
-authorization_code = settings.authorization_code
-           
 
 ############------------ FUNCTION(S) ------------############
-def get_user_info():
-    '''
-     hit endpoint and retreive user info
-    '''
-    response = requests.get(f'https://api.linkedin.com/v2/me?oauth2_access_token={authorization_code}')
-
-    print(response.status_code)
-
+def generate_authorization_url():
+    url = f'https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id={settings.client_id}&redirect_uri={settings.linkedin_redirect}&state={secrets.token_hex(8).upper()}&scope=r_liteprofile%20r_emailaddress%20w_member_social'
+    print(url)
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
-    get_user_info()
+    generate_authorization_url()
