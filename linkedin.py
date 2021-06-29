de936b100457a3f52aa471f894e8dc3d9033693e
aa-ag <aaron.aguerrevere@gmail.com>
2021-06-29 18:39:35 -0500

    was able to retreive an access token -- turns out authorization urls expire pretty quickly and i hadn't noticed. working as expected for now

--- linkedin.py
+++ linkedin.py
@@ -19,6 +19,27 @@ def generate_authorization_url():
     f = 'scope=r_liteprofile%20r_emailaddress%20w_member_social'
     print(a + b + c + d + e + f)
 
+
+def retreive_access_token():
+    access_token_endpoint = 'https://www.linkedin.com/oauth/v2/accessToken?'
+    a = 'grant_type=authorization_code&'
+    b = f'code={settings.authorization_code}&'
+    c = f'redirect_uri={settings.linkedin_redirect}&'
+    d = f'client_id={settings.client_id}&'
+    e = f'client_secret={settings.client_secret}'
+
+    url = access_token_endpoint + a + b + c + d + e
+
+    access_token = requests.post(url).json()
+
+    return access_token
+
+
+
+
+
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
-    generate_authorization_url()
+    # generate_authorization_url()
+
+    print(retreive_access_token())
